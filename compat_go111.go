f0f8f4d34f26bab58d196d48f48306be792eac8b
Roberth Kulbin <roberth@winter-weht.net>
2020-02-02 20:10:09 +0000

    compat: fixup

--- compat_go111.go
+++ compat_go111.go
@@ -2,7 +2,19 @@
 
 package readconf
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+func wrapError(err error, msg string, args ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+
+	args = append(args, err)
+	return fmt.Errorf(msg+": %v", args...)
+}
 
 func stringReplaceAll(s, old, new string) string {
 	return strings.Replace(s, old, new, -1)
